fd7fa8760e6a633af15ff272d344a5c4d21c2b69
Your Name <you@example.com>
2024-03-15 22:05:34 +0500

    #

--- frontend/src/components/Hero/Hero.jsx
+++ frontend/src/components/Hero/Hero.jsx
@@ -1,33 +1,33 @@
-import React from 'react'
-import './Hero.css'
-import hand_icon from "../Assets/hand_icon.png"
-import arrow_icon from "../Assets/arrow.png"
-import hero_img from '../Assets/hero_image.png'
+import React from "react";
+import "./Hero.css";
+import hand_icon from "../Assets/hand_icon.png";
+import arrow_icon from "../Assets/arrow.png";
+import hero_img from "../Assets/hero_image.png";
 const Hero = () => {
-    return (
-        <div className='hero'>
-            <div className="hero-left">
-                <h2>New Arrivals Only</h2>
+  return (
+    <div className="hero">
+      <div className="hero-left">
+        <h2>New Arrivals Only</h2>
 
-                <div>
-                    <div className="haro-hand-icon">
-                        <p>new</p>
-                        <img src={ hand_icon } alt="" />
-                    </div>
-                    <p>Collections</p>
-                    <p>for everyone</p>
-                </div>
-
-                <div className="hero-latest-btn">
-                    <div>Latest Collection</div>
-                    <img src={ arrow_icon } alt="" />
-                </div>
-            </div>
-            <div className="her-right">
-                <img src={ hero_img } alt="" />
-            </div>
+        <div>
+          <div className="haro-hand-icon">
+            <p>new</p>
+            <img src={hand_icon} alt="" />
+          </div>
+          <p>Collections</p>
+          <p>for everyone</p>
         </div>
-    )
-}
 
-export default Hero
+        <div className="hero-latest-btn">
+          <div>Latest Collection</div>
+          <img src={arrow_icon} alt="" />
+        </div>
+      </div>
+      <div className="her-right">
+        <img src={hero_img} alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,83 +1,108 @@
-import React, { useContext, useRef, useState } from 'react'
-import "./Navbar.css"
-import { Link } from "react-router-dom"
-import Logo from "../Assets/logo.png"
-import cart_icon from "../Assets/cart_icon.png"
-import { ShopContext } from '../../Context/ShopContext'
-
-
-
+import React, { useContext, useRef, useState } from "react";
+import "./Navbar.css";
+import { Link } from "react-router-dom";
+import Logo from "../Assets/logo.png";
+import cart_icon from "../Assets/cart_icon.png";
+import { ShopContext } from "../../Context/ShopContext";
 
 const Navbar = () => {
-    const [menu, setMenu] = useState( "shop" )
-    const { getTotalCartItems } = useContext(ShopContext);
-    const menuRef = useRef();
+  const [menu, setMenu] = useState("shop");
+  const { getTotalCartItems } = useContext(ShopContext);
+  const menuRef = useRef();
 
-    const dropdown_toggle = (e) => {
-        menuRef.current.classList.toggle('nav-menu-visible');
-        e.target.classList.toggle('open')
-    }
+  const dropdown_toggle = (e) => {
+    menuRef.current.classList.toggle("nav-menu-visible");
+    e.target.classList.toggle("open");
+  };
 
-    return (
-        <div className='navbar'>
-            <div className='nav-logo'>
-                <img src={Logo} alt="Logo" />
-                <Link to={"/"}>
-                <p>Cosmatics</p> 
-                </Link>
-               
-            </div>
-            {/* dropdown icon */}
-            <i  onClick={dropdown_toggle} class="fa-solid fa-circle-chevron-down   nav-dropdown"></i>
-            {/* dropdown icon */}
-            
-            <ul ref={menuRef} className="nav-menu">
-                <li onClick={ () => { setMenu( "shop" ) } }>
-                    <Link
-                        style={ { textDecoration: "none", color: "#0d0d0d" } }
-                        to="/"> Shop</Link>
-                    { menu === "shop" ? <hr /> : <></> }
-                </li>
-                <li onClick={ () => { setMenu( "Men" ) } }>
-                    <Link
-                        style={ { textDecoration: "none", color: "#0d0d0d" } }
-                        to="/Mens"> Men</Link>
-                    { menu === "Men" ? <hr /> : <></> }
+  return (
+    <div className="navbar">
+      <div className="nav-logo">
+        <img src={Logo} alt="Logo" />
+        <Link to={"/"}>
+          <p>VintageVisage.pk</p>
+        </Link>
+      </div>
+      {/* dropdown icon */}
+      <i
+        onClick={dropdown_toggle}
+        class="fa-solid fa-circle-chevron-down   nav-dropdown"
+      ></i>
+      {/* dropdown icon */}
 
-                </li>
-                <li onClick={ () => { setMenu( "Womens" ) } }
-                >
-                    <Link
-                        style={ { textDecoration: "none", color: "#0d0d0d" } }
-                        to="/Womens"> Women</Link>
-                    { menu === "Womens" ? <hr /> : <></> }
+      <ul ref={menuRef} className="nav-menu">
+        <li
+          onClick={() => {
+            setMenu("shop");
+          }}
+        >
+          <Link style={{ textDecoration: "none", color: "#0d0d0d" }} to="/">
+            {" "}
+            Shop
+          </Link>
+          {menu === "shop" ? <hr /> : <></>}
+        </li>
+        <li
+          onClick={() => {
+            setMenu("Men");
+          }}
+        >
+          <Link style={{ textDecoration: "none", color: "#0d0d0d" }} to="/Mens">
+            {" "}
+            Men
+          </Link>
+          {menu === "Men" ? <hr /> : <></>}
+        </li>
+        <li
+          onClick={() => {
+            setMenu("Womens");
+          }}
+        >
+          <Link
+            style={{ textDecoration: "none", color: "#0d0d0d" }}
+            to="/Womens"
+          >
+            {" "}
+            Women
+          </Link>
+          {menu === "Womens" ? <hr /> : <></>}
+        </li>
+        <li
+          onClick={() => {
+            setMenu("kids");
+          }}
+        >
+          <Link style={{ textDecoration: "none", color: "#0d0d0d" }} to="/kids">
+            {" "}
+            Kids
+          </Link>
+          {menu === "kids" ? <hr /> : <></>}
+        </li>
+      </ul>
+      <div className="nav-login-cart">
+        {localStorage.getItem("auth-token") ? (
+          <button
+            onClick={() => {
+              localStorage.removeItem("auth-token");
+              window.location.replace("/");
+            }}
+          >
+            Logout
+          </button>
+        ) : (
+          <Link to={"/login"}>
+            <button>Login</button>
+          </Link>
+        )}
 
-                </li>
-                <li onClick={ () => { setMenu( "kids" ) } }>
-                    <Link
-                        style={ { textDecoration: "none", color: "#0d0d0d" } }
-                        to="/kids"> Kids</Link>
-                    { menu === "kids" ? <hr /> : <></> }
+        <Link to={"/cart"}>
+          <img src={cart_icon} alt="Add to Cart" />
+        </Link>
 
-                </li>
-            </ul>
-            <div className='nav-login-cart'>
-                {localStorage.getItem('auth-token') ? 
-                    <button onClick={() => { localStorage.removeItem('auth-token');  window.location.replace("/")}}>Logout</button>    
-                    :
-                    <Link to={ "/login" }>
-                    <button>Login</button>
-                </Link>
-            }
-              
-                <Link to={ "/cart" }>
-                    <img src={ cart_icon } alt="Add to Cart" />
-                </Link>
+        <div className="nav-cart-count">{getTotalCartItems()}</div>
+      </div>
+    </div>
+  );
+};
 
-                <div className='nav-cart-count'>{ getTotalCartItems() }</div>
-            </div>
-        </div>
-    )
-}
-
-export default Navbar
+export default Navbar;
